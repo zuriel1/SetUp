6d535df6f098d5e29d44bf8686f503883a8c7aca
Zuriel Williams <zuriel.wz@gmail.com>
2015-07-20 22:27:04 +0000

    Form to bootstrap

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -3,11 +3,6 @@ class QuotesController < ApplicationController
 		@quote = Quote.order("RANDOM() ").first
 	end
 
-
-	def new
-		@quote = Quote.new
-	end
-
 	def create
 		@Quote.create(quote_params)
 		redirect_to root_path
